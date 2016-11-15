00fc75ddd85dc2dc571dc78f84ab185c73e59cd0
sirm2z <m18367819165@gmail.com>
2016-11-15 19:20:49 +0800

    update

--- src/views/account/info.vue
+++ src/views/account/info.vue
@@ -1,6 +1,85 @@
 <template>
   <div class="info">
-    <h1>{{ msg }}</h1>
+    <div class="info-main">
+      <div class="infomation fff_bg_color">
+        <div class="infomation-head">
+          <h3 class="f16">Balance Details</h3>
+        </div>
+        <div class="infomation-body">
+          <div class="infomation-item f14">
+            <div class="item">
+              <label>Advertisers Name: </label>
+              <span class="">JD  （User ID：7777777）</span>
+            </div>
+            <div class="item">
+              <label>Telephone: </label>
+              <span class="">45964768346</span>
+            </div>
+            <div class="item">
+              <label>Email: </label>
+              <span class="">3249637@163.com</span>
+            </div>
+          </div>
+          <div class="infomation-item f14">
+            <div class="item">
+              <label>Password: </label>
+              <span class="">******</span>
+              <a href="">Modify</a>
+            </div>
+            <div class="item">
+              <label>MSN/Facebook: </label>
+              <span class="">405734578（Facebook）</span>
+            </div>
+            <div class="item">
+              <label>Currency: </label>
+              <span class="">USD</span>
+            </div>
+          </div>
+          <div class="infomation-item f14">
+            <div class="item">
+              <label>Contacts: </label>
+              <span class=""></span>
+            </div>
+            <div class="item">
+              <label>Fax Number: </label>
+              <span class=""></span>
+            </div>
+            <div class="item">
+              <label>Site Name: </label>
+              <span class="">JD</span>
+            </div>
+          </div>
+          <div class="infomation-item f14">
+            <div class="item">
+              <label>Site URL: </label>
+              <span class="">http://m.jd.com/</span>
+            </div>
+            <div class="item">
+              <label>Company Name: </label>
+              <span class=""></span>
+            </div>
+            <div class="item">
+              <label>Company Address: </label>
+              <span class="">JD</span>
+            </div>
+          </div>
+          <div class="infomation-item f14">
+            <div class="item">
+              <label>Review Status: </label>
+              <span class="">Approved</span>
+            </div>
+            <div class="item">
+              <label> </label>
+              <span class=""></span>
+            </div>
+            <div class="item">
+              <label> </label>
+              <span class=""></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +88,6 @@ export default {
   name: 'info',
   data () {
     return {
-      msg: 'Welcome to Your info page',
     }
   },
   methods:{
@@ -18,4 +96,10 @@ export default {
 </script>
 
 <style scoped>
+.info{
+  width: 100%;
+}
+.info-main{
+  margin: 10px 30px;
+}
 </style>
